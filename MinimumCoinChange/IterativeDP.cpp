480d313cb20be69e2e1b28259ce86d12375794a5
Rishabh <rishabhsoni3006@gmail.com>
2020-08-18 23:31:00 +0530

    Minimum Coins needed-Recursion changing variable name

--- MinimumCoinChange/IterativeDP.cpp
+++ MinimumCoinChange/IterativeDP.cpp
@@ -3,12 +3,12 @@
 #include<climits>
 using namespace std;
 
-int minimumCoins(int* coin, int n, int sum){
-    if(n<0||sum<0){ //edge case
+int minimumCoins(int* coin, int n, int value){
+    if(n<0||value<0){ //edge case
         return 0;
     }
 
-    if(sum==0){
+    if(value==0){
         return 0;  //as number of coins asked
     }
 
@@ -16,12 +16,12 @@ int minimumCoins(int* coin, int n, int sum){
         return INT_MAX-100;  //1+karenge to limit se exceed ho sakta hai
     }
 
-    if(coin[n-1]<=sum){
-        int option1=1+minimumCoins(coin,n,sum-coin[n-1]);
-        int option2=minimumCoins(coin,n-1,sum);
+    if(coin[n-1]<=value){
+        int option1=1+minimumCoins(coin,n,value-coin[n-1]);
+        int option2=minimumCoins(coin,n-1,value);
         return min(option1,option2);
     }else{
-         return minimumCoins(coin,n-1,sum);
+         return minimumCoins(coin,n-1,value);
     }
 
 }
@@ -29,7 +29,7 @@ int minimumCoins(int* coin, int n, int sum){
 int main(){
     int coin[]={25,10,15};
     int n=3;
-    int sum=30;
-    int ans=minimumCoins(coin,n,sum);
+    int value=30;
+    int ans=minimumCoins(coin,n,value);
     cout<<ans<<endl;
 }
